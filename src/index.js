65c83bc76c1203c8f3005ab90541493b056d7090
Minh Quan <150663837+mrorabang@users.noreply.github.com>
2023-12-08 03:25:01 +0700

    final

--- src/index.js
+++ src/index.js
@@ -7,7 +7,6 @@ import reportWebVitals from './reportWebVitals';
 import { HashRouter } from 'react-router-dom';
 import "@fortawesome/fontawesome-free/css/all.min.css";
 
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <HashRouter>
@@ -15,7 +14,4 @@ root.render(
   </HashRouter>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
